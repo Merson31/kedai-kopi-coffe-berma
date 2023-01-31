300cad499f5ed6f9381a2af35c84f17bb703528a
Merson <mersonmuhammad@gmail.com>
2023-01-31 22:18:13 +0800

    membuat website kedai kopi coffee berma

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle classs active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu diklik
+window.onload = () => {
+  document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+  };
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
